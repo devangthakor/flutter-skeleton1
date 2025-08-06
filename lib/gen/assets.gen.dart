ce6d0a2c3d0598c124b72861cdbe01cf29f8e971
virals <virals@solguruz.com>
2025-08-06 19:08:28 +0530

    adding certificates and their base64 file that is used in CI for Signed IPA

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -182,13 +182,9 @@ class $AssetsTestImagesGen {
 class Assets {
   const Assets._();
 
-  static const String aEnv = '.env';
   static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsTestGen test = $AssetsTestGen();
-
-  /// List of all assets
-  static List<String> get values => [aEnv];
 }
 
 class AssetGenImage {
